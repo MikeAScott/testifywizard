b1b37056e87e8d0f7826f65baf57dbda667efb0c
sqsmike <sqsmike@abe392ae-b7e2-11de-9846-1b058f2a8eb1>
2010-03-19 17:00:50 +0000

    jboss spike wired up authenticator

--- spikes/jboss/HelloWorldProject/HelloWorldProject-ejb/src/main/java/com/sqs/projects/HelloWorldProject/logic/security/AuthenticatorService.java
+++ spikes/jboss/HelloWorldProject/HelloWorldProject-ejb/src/main/java/com/sqs/projects/HelloWorldProject/logic/security/AuthenticatorService.java
@@ -1,17 +1,48 @@
 package com.sqs.projects.HelloWorldProject.logic.security;
 
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+
+import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.Logger;
 import org.jboss.seam.log.Log;
+import org.jboss.seam.security.Credentials;
+import org.jboss.seam.security.Identity;
+
+import com.sqs.projects.HelloWorldProject.model.User;
 
 @Name("authenticator")
 public class AuthenticatorService {
 
 	@Logger
 	protected Log log;
+	@In
+	EntityManager entityManager;
+	@In
+	Credentials credentials;
+	@In
+	Identity identity;
 
 	public boolean authenticate() {
-		log.info("authenticate");
-		return true;
+
+		try {
+
+			User user = (User) entityManager
+					.createQuery(
+							"from User where username = :username and password = :password")
+					.setParameter("username", credentials.getUsername())
+					.setParameter("password", credentials.getPassword())
+					.getSingleResult();
+
+/*			if (user.getRoles() != null) {
+				for (UserRole mr : user.getRoles())
+					identity.addRole(mr.getName());
+			}
+*/
+			return true;
+		} catch (NoResultException ex) {
+			return false;
+		}
 	}
 }
