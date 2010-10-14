5138a717f749b09f352b9e77ba1efd85c6ba8931
sqsmike <sqsmike@abe392ae-b7e2-11de-9846-1b058f2a8eb1>
2010-10-14 16:36:50 +0000

    Updated to use Fitnesse 20100303 Added build.properties to ant build Using jUnit runner for fitnesse suites

--- trunk/src/Resources/skeleton/Common/FitNesse/FitNesse/StartFitNesseServer.bat
+++ trunk/src/Resources/skeleton/Common/FitNesse/FitNesse/StartFitNesseServer.bat
@@ -1,8 +1,7 @@
 @echo off
 setlocal
 set port=%1
-start "FitNesse Server" /min java -Xrs -cp fitnesse.jar;RichNesse.jar fitnesse.FitNesse -p %port% -e 0 -o %2 %3 %4 %5 %6
+start "FitNesse Server" /min java -jar fitnesse.jar -p %port% -e 0 -o %2 %3 %4 %5 %6
 echo **************************************
 echo * Fitnesse server is running on %port% *
 echo **************************************
-
