87065c2b02eca23e6338b9b0662407b37339f8de
sqsmike <sqsmike@abe392ae-b7e2-11de-9846-1b058f2a8eb1>
2010-10-19 16:14:08 +0000

    Added ability to import templates from zip files as start process to separate testify program from template content.

---
+++ trunk/src/AcceptanceTests/TemplateInstallerAcceptanceTest.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using System.IO;
+using ThoughtWorks.TreeSurgeon.Core.Utils;
+using ThoughtWorks.TreeSurgeon.Core.Configuration;
+using System.Configuration;
+
+namespace SQS.Testify.AcceptanceTests {
+    [TestFixture]
+    public class TemplateInstallerAcceptanceTest   {
+
+        private DirectoryInfo wd;
+        private string resourcesPath;
+
+        [SetUp]
+        public void Setup() {
+            wd = new DirectoryInfo(Directory.GetCurrentDirectory());
+            resourcesPath = Path.Combine(wd.FullName, "resources");
+        }
+
+        [Test]
+        public void CanExtractTemplateFromZip()  {
+            RunTemplateInstaller("sample.zip");
+            CheckFileExistsAndIsBiggerThan("sample.config", 100);
+            CheckFileExistsAndIsBiggerThan("skeleton/Sample/Sample.txt", 100);
+            CheckConfigContainsTemplate("Sample");
+        }
+
+        private void CheckConfigContainsTemplate(string templateName) {
+            TemplateSettings settings = getTemplateSettingsConfig("TestifyConsole.exe.config");
+            Assert.IsNotNull(settings.Templates[templateName]);
+        }
+
+
+        [TearDown]
+        public void TearDown() {
+            string configPath = Path.Combine(resourcesPath,"sample.config");
+            string skeletonSample = Path.Combine(resourcesPath, Path.Combine("skeleton", "Sample"));
+            string templateSample = Path.Combine(resourcesPath, Path.Combine("templates", "Sample"));
+            if (Directory.Exists(skeletonSample))
+                Directory.Delete(skeletonSample, true);
+            if (Directory.Exists(templateSample))
+                Directory.Delete(templateSample, true);
+            if (File.Exists(configPath))
+                File.Delete(configPath);
+        }
+
+
+        private void RunTemplateInstaller(string fileName) {
+            // This working dir stuff here for TREE-13
+            ProcessInfo pi = new ProcessInfo("TestifyConsole.exe", "-i " + fileName);
+            ProcessResult installerResult = new ProcessExecutor().Execute(pi);
+            if (installerResult.ExitCode != 0) {
+                Console.WriteLine("TestifyConsole failed");
+                Console.WriteLine("** Standard out from TestifyConsole.exe: **");
+                Console.WriteLine(installerResult.StandardOutput);
+                Console.WriteLine("** End of Standard out from TestifyConsole.exe: **\n");
+                Console.WriteLine("Standard err:");
+                Console.WriteLine(installerResult.StandardError);
+                Assert.Fail("TestifyConsole console app failed");
+            }
+        }
+
+        private void CheckFileExistsAndIsBiggerThan(string fileName, int fileSize) {
+            string expectedLocation = Path.Combine(resourcesPath, fileName);
+            Assert.IsTrue(File.Exists(expectedLocation));
+            Assert.IsTrue(new FileInfo(expectedLocation).Length > fileSize);
+        }
+
+
+        private static TemplateSettings getTemplateSettingsConfig(String configFile) {
+            var map = new ExeConfigurationFileMap { ExeConfigFilename = configFile };
+            Configuration config = ConfigurationManager.OpenMappedExeConfiguration(map, ConfigurationUserLevel.None);
+            TemplateSettings templates = (TemplateSettings)config.GetSection("templateConfiguration/templateSettings");
+            return templates;
+        }
+
+
+    }
+}
--- trunk/src/TestifyConsole/TestifyConsoleMain.cs
+++ trunk/src/TestifyConsole/TestifyConsoleMain.cs
@@ -2,6 +2,7 @@ using System;
 using System.IO;
 using System.Reflection;
 using ThoughtWorks.TreeSurgeon.Core;
+using SQS.Testify.Core;
 
 namespace SQS.Testify.TestifyConsole
 {
@@ -35,25 +36,39 @@ namespace SQS.Testify.TestifyConsole
 			Console.WriteLine("Copyright (C) 2005 Mike Roberts, ThoughtWorks, Inc");
 			Console.WriteLine("Copyright (C) 2008 Mike Scott, SQS");
 			Console.WriteLine();
+            String wd = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
 			if (args.Length < 1)
 			{
 				Usage();
 				return -1;
 			}
-            string version = (args.Length > 1)?args[1]:"2005";
-			Console.WriteLine("Starting Tree Generation for " + args[0]);
-			Console.WriteLine();
-			string outputDirectory = new TreeSurgeonFrontEnd(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), version).GenerateDevelopmentTree("Testify",args[0],version); 
-			Console.WriteLine("Tree Generation complete. Files can be found at " + outputDirectory);
-			return 0;
+            if ("-i".Equals(args[0])) {
+                if (args.Length <2) {
+                    Usage();
+                    return -1;
+                }
+                new TemplateInstaller(wd, Path.Combine(wd, "resources")).Install(args[1]);
+                Console.WriteLine("Template " + args[1] + " installed");
+                return 0;
+
+            } else {
+                string version = (args.Length > 1)?args[1]:"VS2008";
+			    Console.WriteLine("Starting Tree Generation for " + args[0]);
+			    Console.WriteLine();
+			    string outputDirectory = new TreeSurgeonFrontEnd(wd, version).GenerateDevelopmentTree("Testify",args[0],version); 
+			    Console.WriteLine("Tree Generation complete. Files can be found at " + outputDirectory);
+			    return 0;
+            }
 		}
 
 		private static void Usage()
 		{
 			Console.WriteLine("Creates a .NET Development tree");
 			Console.WriteLine();
-			Console.WriteLine("Testify projectName");
-			Console.WriteLine();
+			Console.WriteLine("TestifyConsole projectName template");
+            Console.WriteLine("or");
+            Console.WriteLine("TestifyConsole -i template.zip (install a new template from a zip");
+            Console.WriteLine();
 			Console.WriteLine("Please note - project name must not contain spaces. We recommend you use CamelCase for project names.");
 		}
 	}
